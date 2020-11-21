40238ada3713d4d1a7e983ab5591ffe8940dc1e3
Jonathan <jonathan.sendiko@gmail.com>
2020-11-22 03:51:55 +0700

    home redirection

--- app/src/main/java/com/tutorin/app/student/HomeFragment.kt
+++ app/src/main/java/com/tutorin/app/student/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.tutorin.app.student
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tutorin.app.R
+import com.tutorin.app.business
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -14,6 +17,10 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        buttonBusiness.setOnClickListener {
+            val moveToSubjects = Intent(activity, business::class.java)
+            activity?.startActivity(moveToSubjects)
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
