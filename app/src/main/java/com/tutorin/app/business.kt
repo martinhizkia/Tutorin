a64fd8949b6dcccac92711ef56c2a5bacf90c049
Jonathan <jonathan.sendiko@gmail.com>
2020-11-23 00:34:57 +0700

    Button Homepage

--- app/src/main/java/com/tutorin/app/business.kt
+++ app/src/main/java/com/tutorin/app/business.kt
@@ -2,15 +2,19 @@ package com.tutorin.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tutorin.app.adapter.businessAdapter
 import kotlinx.android.synthetic.main.activity_business.*
 
 class business : AppCompatActivity() {
     private var listJudul = mutableListOf<String>("Dasar Berbisnis", "Kewirausahawan", "Bisnis 1", "Bisnis 2", "Bisnis 3", "Bisnis 4")
+
+    val topicName =intent.getStringExtra("Topics")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_business)
+        topicsName
         businessRecyclerView.adapter = businessAdapter(listJudul)
         businessRecyclerView.layoutManager = LinearLayoutManager(this)
         businessRecyclerView.setHasFixedSize(true)
--- app/src/main/java/com/tutorin/app/student/HomeFragment.kt
+++ app/src/main/java/com/tutorin/app/student/HomeFragment.kt
@@ -2,13 +2,16 @@ package com.tutorin.app.student
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tutorin.app.R
 import com.tutorin.app.business
+import kotlinx.android.synthetic.main.activity_business.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -17,12 +20,41 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        buttonBusiness.setOnClickListener {
-            val moveToSubjects = Intent(activity, business::class.java)
-            activity?.startActivity(moveToSubjects)
-        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
+
+        view.buttonBusiness.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "Business")
+            startActivity(intent)
+        }
+        view.buttonEngineering.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "Engineering")
+            startActivity(intent)
+        }
+        view.buttonHumanities.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "Humanities")
+            startActivity(intent)
+        }
+        view.buttonMedicals.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "Medicals")
+            startActivity(intent)
+        }
+        view.buttonIT.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "IT & Development")
+            startActivity(intent)
+        }
+        view.buttonLaws.setOnClickListener {
+            val intent=Intent(activity,business::class.java)
+            intent.putExtra("Topics", "Laws")
+            startActivity(intent)
+        }
+
+        return view
     }
 
 
