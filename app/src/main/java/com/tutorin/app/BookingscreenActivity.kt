b94045e22add4b769aec6b7f0a3adfca1142ca10
jonathanSendiko <jonathan.sendiko@gmail.com>
2020-12-14 20:48:52 +0700

    Activity Booking

---
+++ app/src/main/java/com/tutorin/app/BookingscreenActivity.kt
@@ -0,0 +1,11 @@
+package com.tutorin.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookingscreen)
+    }
+}
--- app/src/main/java/com/tutorin/app/Splashscreen.kt
+++ app/src/main/java/com/tutorin/app/Splashscreen.kt
@@ -4,16 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.core.os.postDelayed
-import kotlinx.coroutines.delay
 
-class splashscreen : AppCompatActivity() {
+class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
         supportActionBar?.hide()
         Handler().postDelayed({
-            val intent = Intent(this@splashscreen, Home::class.java)
+            val intent = Intent(this@Splashscreen, Home::class.java)
             startActivity(intent)
         }, 3000)
     }
