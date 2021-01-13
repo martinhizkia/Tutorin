93466c26d7603e55d0b8f5f19bc542d744791e90
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-01-14 02:44:30 +0700

    UI repair

--- app/src/main/java/com/tutorin/app/Splashscreen.kt
+++ app/src/main/java/com/tutorin/app/Splashscreen.kt
@@ -6,13 +6,16 @@ import android.os.Bundle
 import android.os.Handler
 
 class Splashscreen : AppCompatActivity() {
+
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
         supportActionBar?.hide()
         Handler().postDelayed({
-            val intent = Intent(this@Splashscreen, Home::class.java)
+            val intent = Intent(this@Splashscreen, Login::class.java)
             startActivity(intent)
+            finish()
         }, 3000)
     }
 }
